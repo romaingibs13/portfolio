8f6947f7fee208ae84f0b62a1ad30db6f85c41a6
John Coder <johncoder@romains-macbook-air.home>
2021-07-14 13:39:08 +0200

    bakground sized to screen

--- app/javascript/Components/MyPage.tsx
+++ app/javascript/Components/MyPage.tsx
@@ -1,21 +1,65 @@
 import * as React from 'react';
+import MyCalendar from './MyCalendar';
 import MyMenu from './MyMenu';
 
 interface MyPageProps {
-  name: string
+
 }
 
-const styles = {
-  
+enum Location {
+  ABOUT = "about",
+  APPOINTMENT = "appointment",
+  CONTACT = "contact"
 }
 
 const MyPage = (props: MyPageProps): React.ReactElement => {
-
+  const [location, setLocation] = React.useState(Location.ABOUT)
+  
   return (
-    <div className="container" >
+    <div className="container">
+      <img height={window.screen.availHeight} width={window.screen.availWidth} src="/assets/background2.jpeg" alt="background image"/>
       <MyMenu/>
+      {/* {location == Location.APPOINTMENT  && <MyCalendar/>} */}
+      <MyCalendar/>
     </div>
   );
+}
+
+function resizeImage(file:File, maxWidth:number, maxHeight:number):Promise<Blob> {
+  return new Promise((resolve, reject) => {
+      let image = new Image();
+      image.src = URL.createObjectURL(file);
+      image.onload = () => {
+          let width = image.width;
+          let height = image.height;
+          
+          if (width <= maxWidth && height <= maxHeight) {
+              resolve(file);
+          }
+
+          let newWidth;
+          let newHeight;
+
+          if (width > height) {
+              newHeight = height * (maxWidth / width);
+              newWidth = maxWidth;
+          } else {
+              newWidth = width * (maxHeight / height);
+              newHeight = maxHeight;
+          }
+
+          let canvas = document.createElement('canvas');
+          canvas.width = newWidth;
+          canvas.height = newHeight;
+
+          let context = canvas.getContext('2d');
+
+          context.drawImage(image, 0, 0, newWidth, newHeight);
+
+          canvas.toBlob(resolve, file.type);
+      };
+      image.onerror = reject;
+  });
 }
 
 export default MyPage;
--- app/javascript/packs/hello_typescript.tsx
+++ app/javascript/packs/hello_typescript.tsx
@@ -4,6 +4,22 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom'
 import MyPage from '../Components/MyPage';
 
+// document.getElementById('input').addEventListener('change', (o: any) => {
+// //If you don't need to resize the image, you can get the blob to upload from the 
+// //FileList (e.g. doUpload(o.target.files[0]);
+
+// // if(o.target.files.length > 0) {
+//   // resizeImage(o.target.files[0], 200, 200).then(blob => {
+//   resizeImage(o.target.files[0], 200, 200) 
+//     //You can upload the resized image: doUpload(blob)
+//     // document.getElementById('img').src = URL.createObjectURL(blob);
+//   // }, err => {
+//     // console.error("Photo error", err);
+//   // });
+// // }});
+
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<MyPage name="React Typescript" />, document.body.appendChild(document.createElement('div')));
+  ReactDOM.render(<MyPage />, document.body.appendChild(document.createElement('div')));
+
+
 });
